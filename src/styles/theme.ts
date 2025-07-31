3c78e5514443e0723ec146a9e5ad230139c6d1e2
dranchukroma <roman2003dranchuk@gmail.com>
2025-07-31 19:06:44 +0200

    Configurated theme file

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,48 @@
 const theme = {
-
-}
+  fontFamily: {
+    logo: "Istok Web",
+    heading: "Istok Web",
+    paragraf: "Istok Web",
+  },
+  fontWeight: {
+    logo: "30px",
+    bigHeading: "50px",
+    mediumHeading: "30px",
+    smallHeading: "25px",
+    bigParagraf: "24px",
+    mediumParagraf: "20px",
+    smallParagraf: "16px",
+    bigButton: "30px",
+    mediumButton: "30px",
+    smallButton: "30px",
+  },
+  fontSize: {
+    bold: "700",
+    regular: "500",
+    light: "300",
+  },
+  color: {
+    fontWhite: "#EEE",
+    fontBlack: "#000",
+    toggleBackground: "#4242429E",
+    uiGradient: {
+      base: "#F3E2D4",
+      gradient:
+        "linear-gradient(180deg,rgba(243, 226, 212, 1) 0%, rgba(244, 233, 225, 1) 100%)",
+    },
+  },
+  background: {
+    bg1: {
+      base: "#C5B0CD",
+      gradient:
+        "radial-gradient(circle,rgba(197, 176, 205, 1) 0%, rgba(161, 97, 185, 1) 100%)",
+    },
+  },
+  breakpoints: {
+    minWidth: "300px",
+    maxWidth: "500px",
+    padding: "15px",
+  },
+};
 
 export default theme;
